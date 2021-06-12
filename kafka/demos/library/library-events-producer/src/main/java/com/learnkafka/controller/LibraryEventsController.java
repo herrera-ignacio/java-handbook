df39944bff8de908b5df2a561a2bf16220f049d0
Ignacio Herrera <ignacioromanherrera@gmail.com>
2021-06-12 19:12:31 -0300

    Kafka/Spring Boot demo PUT endpoint

--- kafka/demos/library/library-events-producer/src/main/java/com/learnkafka/controller/LibraryEventsController.java
+++ kafka/demos/library/library-events-producer/src/main/java/com/learnkafka/controller/LibraryEventsController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
@@ -30,5 +31,18 @@ public class LibraryEventsController {
         this.libraryEventProducer.sendLibraryEventWithSpecificTopic(libraryEvent);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
+    }
+
+    @PutMapping("/v1/libraryevent")
+    public ResponseEntity<?> putLibraryEvent(@RequestBody @Valid LibraryEvent libraryEvent) throws JsonProcessingException {
+        if (libraryEvent.getLibraryEventId() == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Please send the LibraryEventId");
+        }
+
+        libraryEvent.setLibraryEventType(LibraryEventType.UPDATE);
+
+        libraryEventProducer.sendLibraryEventWithSpecificTopic(libraryEvent);
+
+        return ResponseEntity.status(HttpStatus.OK).body(libraryEvent);
     }
 }
